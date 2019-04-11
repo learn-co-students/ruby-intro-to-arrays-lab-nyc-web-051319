b2993ec128d8c94d81020dcce0ea4d1931973ad5
Elma Rahman <elmarahman635@gmail.com>
2019-04-11 23:48:38 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,50 @@
+
+def instantiate_new_array
+  @my_new_array = []
+end
+
+def creates_a_new_array_and_returns_it
+my_new_array = ["cat", "dog", "mice","spider"]
+end
+
+def returns_an_array_that_is_empty
+[]
+end
+
+def array_with_two_elements
+animals = ["lizard" , "bird"]
+end
+
+def vacation_house(array)
+  @vacation_house = ["bbq", "beach","umbrella", "crabs"]
+end
+
+def first_element(one)
+  one[0]
+end
+
+def third_element(three)
+ three[2]
+ end
+
+def last_element(last)
+   last[-1]
+end
+
+def grocery
+@groceries = ["bread", "milk", "eggs", "meat"]
+end
+
+def first_element_with_array_methods(array)
+array[0]
+end
+
+
+def last_element_with_array_methods(array)
+array[-1]
+end
+
+
+def length_of_array(length)
+  length.size
+end
