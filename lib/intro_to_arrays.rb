6637c1905b1adb3596a5bfd65c2c41c8eb2fe63a
Christopher Yuan <chrisyuaners@gmail.com>
2019-04-28 15:29:57 -0500

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,11 +19,11 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  first_element = array.shift
+  array.first
 end
 
 def last_element_with_array_methods(array)
-  last_element = array.pop
+  array.last
 end
 
 def length_of_array(array)
