4102c7cb70a3b9af2abfa1746193a2f624c75f15
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-08 07:12:22 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,46 @@
+def instantiate_new_array
+  new_array=[]
+  @new_array=new_array
+end
+
+def array_with_two_elements
+  new_array=[1,2]
+  @new_array=new_array
+end 
+
+def first_element(array)
+  @array=array.first()
+end
+
+def third_element(array)
+  
+  @array=array[2]
+end
+
+def last_element(array)
+  @array=array.last()
+end
+
+def first_element_with_array_methods(array)
+  @array=array.first()
+end
+
+def last_element_with_array_methods(array)
+  return array.last()
+end
+  def length_of_array(array)
+    return array.length()
+  end
+  
+  
+  
+  
+
+
+
+
+
+  
+  
+  
+  
